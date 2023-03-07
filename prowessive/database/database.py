3fbaff25239f1ffc47074c65f67399635fde335e
Rubén Pozo <rubenpozomolina@gmail.com>
2023-03-07 21:11:37 +0100

    Added methods to insert files to database

--- prowessive/database/database.py
+++ prowessive/database/database.py
@@ -1,6 +1,21 @@
 import os
-
 import psycopg2
+
+FIRST_ITEM = 0
+
+query_table_exists = """
+SELECT EXISTS (
+    SELECT FROM 
+        pg_tables
+    WHERE 
+        schemaname = 'public' AND 
+        tablename  = %s
+    );
+"""
+
+create_files_table = """
+CREATE TABLE files (path text, file_bytes bytea);
+"""
 
 
 class Database:
@@ -29,8 +44,44 @@ class Database:
                 f"dbname={self.database} user={self.user} password={self.password} host={self.host} port={self.port}"
             )
             self.default_cursor = self.connection.cursor()
+            self.create_table_files()
         except Exception as e:
             print("Error connecting to database:", str(e))
+
+    def query(self, sql, variables=None):
+        cursor = self.connection.cursor()
+        cursor.execute(sql, variables)
+        return_value = cursor.fetchall()
+        cursor.close()
+        return return_value
+
+    def execute(self, sql, variables=None):
+        self.default_cursor.execute(sql, variables)
+
+    def insert_file(self, uri_path, path_to_file):
+        try:
+            binary_file = open(path_to_file, 'rb').read()
+            self.execute(
+                "INSERT INTO files (path, file_bytes) VALUES (%s, %s)",
+                (uri_path, psycopg2.Binary(binary_file))
+            )
+            self.connection.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    def create_table_files(self):
+        query_result = self.query(query_table_exists, ('files',))
+        table_files_exists = query_result[FIRST_ITEM][FIRST_ITEM]
+        if not table_files_exists:
+            self.execute(create_files_table)
+
+    def delete_file(self, uri_path, path_to_dir):
+        try:
+            self.execute("SELECT file_bytes FROM files WHERE path = %s", uri_path)
+            blob = self.default_cursor.fetchone()
+            open(path_to_dir + blob[0] + '.' + blob[1], 'wb').write(blob[2])
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
 
     def close_connection(self):
         if self.connection:
--- tests/integration/test_database.py
+++ tests/integration/test_database.py
@@ -1,12 +1,34 @@
+import pytest
+
 from prowessive.database.database import Database
+
+create_table = """
+CREATE TABLE test_files (file_name text, file_bytes bytea);
+"""
+
+drop_table = """
+DROP TABLE test_files;
+"""
+
+
+@pytest.fixture(scope="session", autouse=True)
+def database(configuration):
+    local_database = Database()
+    local_database.execute(drop_table)
+    local_database.execute(create_table)
+    yield local_database
 
 
 class TestDatabase:
-    def test_connection(self, configuration):
-        database = Database()
+    def test_connection(self, database):
         assert database.connection
 
-    def test_get_version(self, configuration):
-        database = Database()
+    def test_get_version(self, database):
         version = database.connection.server_version
         assert version == 140001
+
+    def test_database_file(self, database):
+        database.insert_file('/test/index.html', 'tests/files/index.html')
+        files = database.query("SELECT path, file_bytes from files")
+        print(files)
+        assert len(files) > 0
