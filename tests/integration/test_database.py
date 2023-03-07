eb7ff3330760d7c86198de046fde8561a6d4611e
Rubén Pozo <rubenpozomolina@gmail.com>
2023-03-07 18:00:27 +0100

    Added test to verify postgres version

--- tests/integration/test_database.py
+++ tests/integration/test_database.py
@@ -5,3 +5,8 @@ class TestDatabase:
     def test_connection(self, configuration):
         database = Database()
         assert database.connection
+
+    def test_get_version(self, configuration):
+        database = Database()
+        version = database.connection.server_version
+        assert version == 140001
